5701c8ac5703a87eb4e43d5aaa736f0fb57ce4b7
mgreen12 <mgreen12@L11707042770.wctc.msft>
2014-09-02 14:22:39 -0500

    Concrete classes work and demonstrate working inheritance. Ran out of time to finish other packages.

--- src/my/concrete/Main.java
+++ src/my/concrete/Main.java
@@ -8,11 +8,12 @@ public class Main {
     
     public static void main(String[] args) {
         
-
+        // New HourlyEmployee "Jeff" for testing.
         HourlyEmployee employee1 = new HourlyEmployee("Jeff", "112233", 20);
         employee1.setHoursWorked(42);
         employee1.setHourlyPay(8.00);
         
+        //This shows that HourlyEmployee properly inherits methods from Employee, so all other subclasses would work too if I tested them.
         System.out.println(employee1.getName() + " worked " + employee1.getHoursWorked() + " hours at $" + employee1.getHourlyPay() + "/hour\n$" + employee1.getPay() + " earned for the week");
         
 
